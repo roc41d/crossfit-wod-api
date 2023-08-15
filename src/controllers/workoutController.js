84512744bbc723101477292a03f032040825ba4d
Rocard Fonji <fonkengrocard@gmail.com>
2023-08-15 18:45:20 +0100

    add sekeleton crud methods in service layer

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,20 +1,27 @@
+const workoutService = require("../services/workoutService");
+
 const getAllWorkouts = async (req, res) => {
+    const workouts = await workoutService.getAllWorkouts();
     res.send("Get all workouts");
 };
 
 const getWorkoutById = async (req, res) => {
+    const workout = await workoutService.getWorkoutById();
     res.send(`Get workout with id ${req.params.workoutId}`);
 };
 
 const createWorkout = async (req, res) => {
+    const workout = await workoutService.createWorkout();
     res.send("Create new workout");
 };
 
 const updateWorkout = async (req, res) => {
+    const workout = await workoutService.updateWorkout();
     res.send(`Update workout with id ${req.params.workoutId}`);
 };
 
 const deleteWorkout = async (req, res) => {
+    const workout = await workoutService.deleteWorkout();
     res.send(`Delete workout with id ${req.params.workoutId}`);
 };
 
---
+++ src/services/workoutService.js
@@ -0,0 +1,27 @@
+const getAllWorkouts = async () => {
+    return;
+};
+
+const getWorkoutById = async () => {
+    return;
+};
+
+const createWorkout = async () => {
+    return;
+};
+
+const updateWorkout = async () => {
+    return;
+};
+
+const deleteWorkout = async () => {
+    return;
+};
+
+module.exports = {
+    getAllWorkouts,
+    getWorkoutById,
+    createWorkout,
+    updateWorkout,
+    deleteWorkout
+};
